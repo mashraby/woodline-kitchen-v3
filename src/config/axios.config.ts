e2862159d814b01abc50b380364aabf697cc342e
mashrabyuldashov <yoldashovmashrab@gmail.com>
2023-05-23 20:06:28 +0500

    asdkjas

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -3,14 +3,14 @@ import axios, { AxiosInstance } from "axios";
 const token: string | null = window.localStorage.getItem("token");
 
 export const authInstance: AxiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL_PRODUCTION,
+  baseURL: "https://backend4devs.uz",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export const instance: AxiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL_PRODUCTION,
+  baseURL: "https://backend4devs.uz",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${token}`,
